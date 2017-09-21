5a3deb1fedefcc1e147cfa60c20c50e4a1d17770
AustinBaggio <austinbaggio@gmail.com>
2017-09-21 11:46:01 -0400

    Fix Clubbed Button
    wrapped Button in a CardSection

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 //configuring firebase
 class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
     apiKey: 'AIzaSyBGUmii7qKkWjUBXAYW5aWuwjTCNBK03Ss',
@@ -15,13 +17,34 @@ class App extends Component {
     storageBucket: 'authentication-abf77.appspot.com',
     messagingSenderId: '1041917981485'
   });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    if (this.state.loggedIn) {
+      return (
+        <CardSection>
+          <Button>
+            Log Out
+          </Button>
+        </CardSection>
+      );
+    }
+    return <LoginForm />;
   }
 
   render() {
     return (
       <View>
-        <Header headerText="Auth" />
-        <LoginForm />
+        <Header headerText="Authentication" />
+        {this.renderContent()}
       </View>
     );
   }
